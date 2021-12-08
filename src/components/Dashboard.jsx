04b77ebcf56589d6d8cb2bda653f7850f3f29cac
Brandon Gomez <bjgomez567@gmail.com>
2021-12-07 22:48:58 -0500

    Commit inicial de la rama dashboard 2

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import BarChart from './BarChart';
 import '../App.css';
-import { Chart } from 'chart.js';
 
 
 const metricas = [
